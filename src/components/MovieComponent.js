0db43b763efeaf68c3d1319ee89df7a91d61f7b4
vaikailas <137754989+vaikailas@users.noreply.github.com>
2023-08-06 09:42:30 +0530

    first commit

---
+++ src/components/MovieComponent.js
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+
+const MovieContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  width: 280px;
+  box-shadow: 0 3px 10px 0 #aaa;
+  cursor: pointer;
+  background-color: #232323;
+  border-radius: 15px;
+`;
+const CoverImage = styled.img`
+  object-fit: fill;
+  height: 362px;
+  border-radius: 15px;
+
+`;
+const MovieName = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  color: #DDDDDD;
+  margin: 15px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+const InfoColumn = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const MovieInfo = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: #DDDDDD;
+  white-space: nowrap;
+  overflow: hidden;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+  background-color: #232323;
+  
+`;
+const MovieComponent = (props) => {
+  const { Title, Year, imdbID, Type, Poster } = props.movie;
+
+  return (
+    <MovieContainer
+      onClick={() => {
+        props.onMovieSelect(imdbID);
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }}
+    >
+      <CoverImage src={Poster} alt={Title} />
+      <MovieName>{Title}</MovieName>
+      <InfoColumn>
+        <MovieInfo>Year : {Year}</MovieInfo>
+        <MovieInfo>Type : {Type}</MovieInfo>
+      </InfoColumn>
+    </MovieContainer>
+  );
+};
+export default MovieComponent;
